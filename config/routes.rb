6bf9b61129f55e022dbd2b4dfa61da8ad936e28e
khongor <khongor.enkhbold@gmail.com>
2015-01-25 16:03:36 -0800

    Hide registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Vacaybug::Application.routes.draw do
-    devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_up => 'register', :sign_out => 'logout'}
+    devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_up => 'kcw', :sign_out => 'logout'}
 
     resources :friendships
 
