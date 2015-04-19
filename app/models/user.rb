2c35fa15160cad3c5f160ad33e99065c68415b02
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-04-19 10:35:13 -0700

    Devise mailers should work now (dev + prod). Configured devise so that confirmation and forgot password works

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
     # Include default devise modules. Others available are:
     # :token_authenticatable, :confirmable,
     # :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
+    devise :database_authenticatable, :registerable, :confirmable,  
         :recoverable, :rememberable, :trackable, :validatable
 
     # Setup accessible (or protected) attributes for your model
-    attr_accessible :id, :email, :password, :password_confirmation, :remember_me
+    attr_accessible :id, :email, :password, :password_confirmation, :remember_me, :unconfirmed_email
     attr_accessible :website, :location, :photo_url, :tag_line, :username, :first_name, :last_name
     attr_accessible :avatar, :birthday
       has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/default_avatar.jpg"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150418230325) do
+ActiveRecord::Schema.define(:version => 20150419171155) do
 
   create_table "comments", :force => true do |t|
     t.integer  "story_id"
@@ -177,8 +177,12 @@ ActiveRecord::Schema.define(:version => 20150418230325) do
     t.datetime "avatar_updated_at"
     t.string   "username"
     t.date     "birthday"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
