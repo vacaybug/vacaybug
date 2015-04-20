a95b501abd09be657b331d7851ee8b20dd5a4bbc
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-04-19 19:02:10 -0700

    running out of ideas

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,57 +1,135 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config/deploy.rb
+require "bundler/capistrano"
 
-set :application, 'vacaybug'
-set :repo_url, 'git@github.com:khongor/vacaybug.git'
-set :deploy_to, '/home/ubuntu/deploy/apps/vacaybug'
-set :scm, :git
-set :branch, 'master'
-set :keep_releases, 5
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :format, :pretty
-set :log_level, :debug
-set :pty, true
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :scm,             :git
+set :repository,      "git@github.com:capistrano-plugins/capistrano-unicorn-nginx.git"
+set :branch,          "origin/master"
+set :migrate_target,  :current
+set :ssh_options,     { :forward_agent => true }
+set :rails_env,       "production"
+set :deploy_to,       "/home/deploy/apps/vacaybug"
+set :normalize_asset_timestamps, false
 
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
-set :puma_role, :app
-set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [0, 16]
-set :puma_workers, 0
-set :puma_init_active_record, true
-set :puma_preload_app, true
+set :user,            "deployer"
+set :group,           "staff"
+set :use_sudo,        false
 
-set :ssh_options, {
-  forward_agent: true,
-  auth_methods: ["publickey"],
-  keys: ["/Users/khongor/.ssh/vacaybug.pem"]
-}
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+default_environment["RAILS_ENV"] = 'production'
+
+# Use our ruby-1.9.2-p290@my_site gemset
+default_environment["PATH"]         = "--"
+default_environment["GEM_HOME"]     = "--"
+default_environment["GEM_PATH"]     = "--"
+default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
+
+default_run_options[:shell] = 'bash'
 
 namespace :deploy do
+  desc "Deploy your application"
+  task :default do
+    update
+    restart
+  end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  desc "Setup your git-based deployment app"
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
+  end
+
+  task :cold do
+    update
+    migrate
+  end
+
+  task :update do
+    transaction do
+      update_code
     end
   end
 
-  after :publishing, :restart
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
+  end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    transaction do
+      update_code
+    end
+    migrate
+    restart
+  end
+
+  task :finalize_update, :except => { :no_release => true } do
+    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+    # mkdir -p is making sure that the directories are there for some SCM's that don't
+    # save empty folders
+    run <<-CMD
+      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+      mkdir -p #{latest_release}/public &&
+      mkdir -p #{latest_release}/tmp &&
+      ln -s #{shared_path}/log #{latest_release}/log &&
+      ln -s #{shared_path}/system #{latest_release}/public/system &&
+      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+    CMD
+
+    if fetch(:normalize_asset_timestamps, true)
+      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
     end
   end
 
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn.my_site.pid`"
+  end
+
+  desc "Start unicorn"
+  task :start, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+
+  desc "Stop unicorn"
+  task :stop, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn.my_site.pid`"
+  end
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
+  end
+end
+
+def run_rake(cmd)
+  run "cd #{current_path}; #{rake} #{cmd}"
 end
