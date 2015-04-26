c3f16384735b05f02e1e1d9a3b1dfc4995740cdd
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-04-26 11:47:00 -0700

    disable oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,37 +1,37 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-		# You need to implement the method below in your model (e.g. app/models/user.rb)
-		@user = User.from_omniauth_facebook(request.env["omniauth.auth"])
+	# def facebook
+	# 	# You need to implement the method below in your model (e.g. app/models/user.rb)
+	# 	@user = User.from_omniauth_facebook(request.env["omniauth.auth"])
 
-		if @user.persisted?
-			sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-			set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-		else
-			redirect_to new_user_registration_url
-		end
-	end
+	# 	if @user.persisted?
+	# 		sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+	# 		set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+	# 	else
+	# 		redirect_to new_user_registration_url
+	# 	end
+	# end
 
-	def twitter
-		# You need to implement the method below in your model (e.g. app/models/user.rb)
-		@user = User.from_omniauth_twitter(request.env["omniauth.auth"])
+	# def twitter
+	# 	# You need to implement the method below in your model (e.g. app/models/user.rb)
+	# 	@user = User.from_omniauth_twitter(request.env["omniauth.auth"])
 
-		if @user.persisted?
-			sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-			set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-		else
-			redirect_to new_user_registration_url
-		end
-	end
+	# 	if @user.persisted?
+	# 		sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+	# 		set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+	# 	else
+	# 		redirect_to new_user_registration_url
+	# 	end
+	# end
 
-	def google_oauth2
-		# You need to implement the method below in your model (e.g. app/models/user.rb)
-		@user = User.from_omniauth_google(request.env["omniauth.auth"])
+	# def google_oauth2
+	# 	# You need to implement the method below in your model (e.g. app/models/user.rb)
+	# 	@user = User.from_omniauth_google(request.env["omniauth.auth"])
 
-		if @user.persisted?
-			sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-			set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-		else
-			redirect_to new_user_registration_url
-		end
-	end
+	# 	if @user.persisted?
+	# 		sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+	# 		set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+	# 	else
+	# 		redirect_to new_user_registration_url
+	# 	end
+	# end
 end
