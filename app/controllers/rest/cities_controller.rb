0477c3b01cbf4ed28dede0ffa14029b8618f90b5
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-06-21 17:54:03 -0700

    remove the limit

--- app/controllers/rest/cities_controller.rb
+++ app/controllers/rest/cities_controller.rb
@@ -7,7 +7,7 @@ class Rest::CitiesController < ActionController::Base
         guides = Guide.joins(:places).group('guide_id').having('count(*) >= 3').to_sql
         ('A'..'Z').each do |group|
             city_ids = City.where('cities.city LIKE ?', "#{group}%").select('gn_id, COUNT(g.geonames_id) as count').
-                joins("join (#{guides}) g on g.geonames_id=cities.gn_id").group('cities.gn_id').order('count desc').limit(7).
+                joins("join (#{guides}) g on g.geonames_id=cities.gn_id").group('cities.gn_id').order('count desc').
                 map { |c| c.gn_id }
             response[group] = City.where(gn_id: city_ids).order('city asc')
         end
