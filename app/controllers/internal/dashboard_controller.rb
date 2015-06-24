80cad299752222b17b57f5ced36745fc329ce7ed
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-06-23 20:49:23 -0700

    updated dashboard

--- app/controllers/internal/dashboard_controller.rb
+++ app/controllers/internal/dashboard_controller.rb
@@ -10,9 +10,16 @@ class Internal::DashboardController < ActionController::Base
     end
 
     def users
-    	@groups = User.select(
-    		'date(created_at) date, GROUP_CONCAT(id) as ids'
-    	).group('date(created_at)').map { |u| [u.date, u.ids] }
+    	@users = User.all
+
+    	@grouped_cities = {}
+    	guides = Guide.joins(:places).group('guide_id').having('count(*) >= 3 AND guide_type = 1').to_sql
+    	('A'..'Z').each do |group|
+    	    city_ids = City.where('cities.city LIKE ?', "#{group}%").select('gn_id, COUNT(g.geonames_id) as count').
+    	        joins("join (#{guides}) g on g.geonames_id=cities.gn_id").group('cities.gn_id').order('count desc').
+    	        map { |c| c.gn_id }
+    	    @grouped_cities[group] = City.where(gn_id: city_ids).order('city asc')
+    	end
 
     	render 'internal/dashboard/users'
     end
