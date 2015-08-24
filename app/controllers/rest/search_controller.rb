d915f795039cf161b40874d2edc93e28badd4e4c
Khongor Enkhbold <khongor.enkhbold@gmail.com>
2015-08-23 17:28:38 -0700

    only show guides if they have more than 3 places

--- app/controllers/rest/search_controller.rb
+++ app/controllers/rest/search_controller.rb
@@ -5,13 +5,13 @@ class Rest::SearchController < ActionController::Base
 
     def search_recent geonames_id
     	render json: {
-            models: Guide.where(geonames_id: geonames_id, guide_type: Guide::TYPES::PASSPORT).order('id desc').limit(50)
+            models: Guide.where(geonames_id: geonames_id).joins(:places).group('guide_id').having('count(*) >= 3 AND guide_type = 1').order('id desc').limit(50)
         }
     end
 
     def search_popular geonames_id
     	render json: {
-    		models: Guide.where(geonames_id: geonames_id, guide_type: Guide::TYPES::PASSPORT).order('popularity desc').limit(50)
+            models: Guide.where(geonames_id: geonames_id).joins(:places).group('guide_id').having('count(*) >= 3 AND guide_type = 1').order('popularity desc').limit(50)
     	}
     end
 
